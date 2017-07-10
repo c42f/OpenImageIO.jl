e5ba977a1e52ed0d8a7a2a48c0f982348e14e8ca
Chris Foster <chris42f@gmail.com>
2017-07-11 08:50:38 +1000

    Type conversion for string_view

--- deps/src/jl_oiio.h
+++ deps/src/jl_oiio.h
@@ -17,4 +17,46 @@ void register_ImageInput(jlcxx::Module& mod);
 
 } // namespace JlOpenImageIO
 
+//------------------------------------------------------------------------------
+// CxxWrap type conversion machinery for OIIO types
+
+namespace jlcxx {
+
+// string_view
+
+template<> struct IsValueType<OIIO::string_view> : std::true_type { };
+
+template<>
+struct static_type_mapping<OIIO::string_view>
+{
+    typedef jl_value_t* type;
+    static jl_datatype_t* julia_type()
+    {
+        return (jl_datatype_t*)jl_get_global(jl_base_module,
+                                             jl_symbol("AbstractString"));
+    }
+};
+
+template<>
+struct ConvertToJulia<OIIO::string_view, false, false, false>
+{
+    jl_value_t* operator()(const OIIO::string_view& str) const
+    {
+        return jl_pchar_to_string(str.data(), str.length());
+    }
+};
+
+template<>
+struct ConvertToCpp<OIIO::string_view, false, false, false>
+{
+    OIIO::string_view operator()(jl_value_t* jstr) const
+    {
+        if (!jl_is_string(jstr))
+            throw std::runtime_error("Type to convert to string is not a string but a " + julia_type_name((jl_datatype_t*)jl_typeof(jstr)));
+        return OIIO::string_view(jl_string_data(jstr), jl_string_len(jstr));
+    }
+};
+
+} // namespace jlcxx
+
 #endif // JLOPENIMAGEIO_JL_OIIO_H
